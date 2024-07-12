6500d00c0cd83b61cc9b0a4c776f4b8b8d2d6302
Daniel Trumpp <dtrumpp@gmail.com>
2024-07-12 03:34:52 +0000

    Initial version

---
+++ config_flow.py
@@ -0,0 +1,74 @@
+"""Config flow for AI image analysis integration."""
+
+from __future__ import annotations
+
+import logging
+from typing import Any
+import re
+
+import voluptuous as vol
+
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_HOST
+from homeassistant.core import HomeAssistant, HomeAssistantError
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required(CONF_HOST): str})
+
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+
+    # If your PyPI package is not built with async, pass your methods
+    # to the executor:
+    # await hass.async_add_executor_job(
+    #     your_validate_func, data[CONF_USERNAME], data[CONF_PASSWORD]
+    # )
+
+    p = "(?:http.*://)(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*"
+
+    result = re.match(p, data[CONF_HOST])
+
+    if not result:
+        raise InvalidUrlScheme
+
+    # Return info that you want to store in the config entry.
+    return {"host": data[CONF_HOST]}
+
+
+class ConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for AI image analysis."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle the initial step."""
+        errors: dict[str, str] = {}
+        if user_input is not None:
+            try:
+                await validate_input(self.hass, user_input)
+            except InvalidUrlScheme:
+                errors["base"] = "invalid_url_scheme"
+            except Exception:
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(
+                    title="AI image analysis", data=user_input
+                )
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+
+class InvalidUrlScheme(HomeAssistantError):
+    """Error to indicate we cannot connect."""
---
+++ const.py
@@ -0,0 +1,3 @@
+"""Constants for the AI image analysis integration."""
+
+DOMAIN = "ai_image_analysis"
